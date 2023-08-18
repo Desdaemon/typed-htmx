57b065e0bde61e4deb55a9636dbff58069376d81
Viet Dinh <54ckb0y789@gmail.com>
2023-08-17 22:43:25 -0400

    chore: bump tests

--- tests/static-jsx/main.test.js
+++ tests/static-jsx/main.test.js
@@ -3,7 +3,7 @@ describe("html templator", () => {
 		expect(<div data-foo={123} />).toBe('<div data-foo="123" />');
 	});
 	it("processes json attributes", () => {
-		expect(<div hx-vals={{ foo: "It's joever" }} />).toBe(`<div hx-vals='{"foo":"It&#39;s joever"}' />`);
+		expect(<div hx-vals={{ foo: "It's joever" }} />).toBe(`<div hx-vals='{"foo":"It&#39;s joever"}'/>`);
 	});
 	it("skips falsy attributes", () => {
 		expect(<div data-foo={false} {...{ foobar: false }} />).toBe("<div />");
